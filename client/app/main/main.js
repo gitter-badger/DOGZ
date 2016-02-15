951ffc107a7a4d031901ab03de6e1fdcffd4a857
Marcin Mrotek <marcin.mrotek@mobica.com>
2016-02-15 14:27:19 -0800

    [ADD] Added legal templates

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -8,5 +8,17 @@ angular.module('dogsApp')
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+      .state('terms', {
+        url: '/terms',
+        templateUrl: 'app/main/terms.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .state('privacy', {
+        url: '/privacy',
+        templateUrl: 'app/main/privacy.html',
+        controller: 'MainController',
+        controllerAs: 'main'
       });
   });
